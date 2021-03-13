1709c2172566e8b242a0f5d878ff7671b42c20f8
Nate Davis Olds <nate@davisolds.com>
2021-03-13 12:35:37 -0500

    Sets next config target to serverless

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
   cssModules: false,
+  target: 'serverless',
   env: {
     stripe_public_key: process.env.STRIPE_PK,
   },
